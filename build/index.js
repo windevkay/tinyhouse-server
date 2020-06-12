5c02a4e23747b45383e0a99272fe461e8c87027f
Administrator <administrator@Kolas-MBP.hitronhub.home>
2020-06-12 11:36:01 -0600

    ts adjustment

--- build/index.js
+++ build/index.js
@@ -1,9 +1,14 @@
 'use strict';
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, '__esModule', { value: true });
-const express = require('express');
+const express_1 = __importDefault(require('express'));
 const apollo_server_express_1 = require('apollo-server-express');
 const graphql_1 = require('./graphql');
-const app = express();
+const app = express_1.default();
 const port = 9000;
 // create apollo server
 const server = new apollo_server_express_1.ApolloServer({ schema: graphql_1.schema });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express = require('express');
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
 import { schema } from './graphql';
