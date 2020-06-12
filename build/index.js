8ada9eb3baebc2e15f5aee3ca4a13b253f4ef0da
Administrator <administrator@Kolas-MBP.hitronhub.home>
2020-06-12 11:35:03 -0600

    apollo setup

--- build/index.js
+++ build/index.js
@@ -1,12 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const express = require('express');
+const apollo_server_express_1 = require('apollo-server-express');
+const graphql_1 = require('./graphql');
+const app = express();
 const port = 9000;
-const message = 'Hello World';
-app.get('/', (req, res) => res.send(message));
+// create apollo server
+const server = new apollo_server_express_1.ApolloServer({ schema: graphql_1.schema });
+server.applyMiddleware({ app, path: '/api' });
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
